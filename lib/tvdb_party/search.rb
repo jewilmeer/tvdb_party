ace409345f2eb78aea93865c931a7ad2e07aed0d
Robert Beekman <robert@matsimitsu.nl>
2011-02-12 18:33:14 +0100

    added find_episode_by_id and update functionaly

--- lib/tvdb_party/search.rb
+++ lib/tvdb_party/search.rb
@@ -26,11 +26,30 @@ module TvdbParty
       end
     end
 
+    def get_series_updates(timestamp)
+      response = self.class.get("/Updates.php", {:query => { :time => timestamp, :type => 'series'}}).parsed_response
+      return response["Items"] ? response["Items"] : []
+    end
+
+    def get_episodes_updates(timestamp)
+      response = self.class.get("/Updates.php", {:query => { :time => timestamp, :type => 'episode'}}).parsed_response
+      return response["Items"] ? response["Items"] : []
+    end
+
     def get_series_by_id(series_id, language = self.language)
       response = self.class.get("/#{@api_key}/series/#{series_id}/#{language}.xml").parsed_response
 
       if response["Data"] && response["Data"]["Series"]
         Series.new(self, response["Data"]["Series"])
+      else
+        nil
+      end
+    end
+
+    def get_episode_by_id(episode_id, language = self.language)
+      response = self.class.get("/#{@api_key}/episodes/#{episode_id}/#{language}.xml").parsed_response
+      if response["Data"] && response["Data"]["Episode"]
+        Episode.new(response["Data"]["Episode"])
       else
         nil
       end
